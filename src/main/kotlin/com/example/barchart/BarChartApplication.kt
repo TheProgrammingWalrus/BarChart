a4569b9abf4b05744cee106d537e0af1d4e89e0c
Charlie K <crknox412@gmail.com>
2022-06-20 16:15:10 -0700

    Version 5 Bar Chart with multiple Data Series

--- src/main/kotlin/com/example/barchart/BarChartApplication.kt
+++ src/main/kotlin/com/example/barchart/BarChartApplication.kt
@@ -2,21 +2,18 @@ package com.example.barchart
 
 import javafx.application.Application
 import javafx.scene.Scene
-import javafx.scene.chart.BarChart
-import javafx.scene.chart.CategoryAxis
-import javafx.scene.chart.NumberAxis
-import javafx.scene.chart.XYChart
+import javafx.scene.chart.*
 import javafx.scene.layout.Pane
 import javafx.stage.Stage
 
-class HelloApplication : Application() {
+class BarChartApplication : Application() {
     override fun start(stage: Stage) {
         // Create x-axis and y-axis
         val xAxis = CategoryAxis()
         val yAxis = NumberAxis()
 
         // create a bar chart
-        val barchart1 = BarChart(xAxis, yAxis)
+        val barchart1 = StackedBarChart(xAxis, yAxis)
         barchart1.title = "Car Sales by Manufacturer"
 
         // create a data series
@@ -26,8 +23,20 @@ class HelloApplication : Application() {
         dataSeries1.data.add(XYChart.Data("GM", 45000))
         dataSeries1.data.add(XYChart.Data("Toyota", 20000))
 
+        val dataSeries2 = XYChart.Series<String, Number>()
+        dataSeries2.name = "February 2022"
+        dataSeries2.data.add(XYChart.Data("Ford", 20000))
+        dataSeries2.data.add(XYChart.Data("GM", 33000))
+        dataSeries2.data.add(XYChart.Data("Toyota", 27000))
+
+        val dataSeries3 = XYChart.Series<String, Number>()
+        dataSeries3.name = "March 2022"
+        dataSeries3.data.add(XYChart.Data("Ford", 30000))
+        dataSeries3.data.add(XYChart.Data("GM", 50000))
+        dataSeries3.data.add(XYChart.Data("Toyota", 46000))
+
         // add the data series to the chart
-        barchart1.data.add(dataSeries1)
+        barchart1.data.addAll(dataSeries1, dataSeries2, dataSeries3)
 
         // create a pane and add the bar chart to it
         val pane = Pane()
@@ -45,5 +54,5 @@ class HelloApplication : Application() {
 }
 
 fun main() {
-    Application.launch(HelloApplication::class.java)
+    Application.launch(BarChartApplication::class.java)
 }
